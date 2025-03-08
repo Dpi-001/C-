7da6bc39539ce630f852019e56099f9f0568dfa9
Dpi-001 <sudip9845197918@gmail.com>
2025-03-08 22:02:58 +0545

    C#

---
+++ arthoperation.cs
@@ -0,0 +1,24 @@
+using System;
+
+
+public class Arthoperation
+{
+public static void Main()
+{
+double num1,num2;
+
+Console.WriteLine("Enter First Number");
+num1=Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter Second Number");
+num2=Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Add:"+(num1+num2));
+Console.WriteLine("Sub:"+(num1-num2));
+Console.WriteLine("Mul:"+(num1*num2));
+Console.WriteLine("Div:"+(num1/num2));
+
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
+}
+}
---
+++ twonumber.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Calculate
+{
+    public static void Main()
+    {
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+        Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}

15a12df87fc17f6ff2fb4401a9081ec8c5731b47
Dpi-001 <sudip9845197918@gmail.com>
2025-03-09 20:07:48 +0545

    Input two number find largest and smallest

---
+++ twosmla.cs
@@ -0,0 +1,35 @@
+using System;
+
+public class Calculate
+{
+    int num1, num2;
+
+    public void GetNumbers()
+    {
+        Console.Write("Enter the first number: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+    }
+
+    public void FindSmallest()
+    {
+        int result = (num1 < num2) ? num1 : num2;
+        Console.WriteLine("The smallest number is " + result);
+    }
+
+    public void FindLargest()
+    {
+        int result = (num1 > num2) ? num1 : num2;
+        Console.WriteLine("The largest number is " + result);
+    }
+
+    public static void Main()
+    {
+        Calculate obj = new Calculate();
+        obj.GetNumbers();
+        obj.FindSmallest();
+        obj.FindLargest();
+    }
+}

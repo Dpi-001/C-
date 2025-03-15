1cf69848cc6cbd0831c63f3288a1a75caa4d7eba
Dpi-001 <sudip9845197918@gmail.com>
2025-03-15 22:44:54 +0545

    Simple show odd even

---
+++ evenodd.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class EvenOdd
+{
+    public static void Main(string[] args)
+    {
+        int number = 10;
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("Even");
+        }
+        else
+        {
+            Console.WriteLine("Odd");
+        }
+    }
+}

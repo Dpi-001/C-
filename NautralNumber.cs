e65abcafd9d2a444077c2fd7b97db0e12713224b
Dpi-001 <sudip9845197918@gmail.com>
2025-03-17 11:17:51 +0545

    Nataural Number upto 25 Iteration

---
+++ NautralNumber.cs
@@ -0,0 +1,16 @@
+// Nautural number using while loop upto 25
+
+using System;
+
+public class NaturalNumber
+{
+    public static void Main(string[] args)
+    {
+        int i = 1;
+        while (i <= 25)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+    }
+} 

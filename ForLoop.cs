2202d1894f56bfa20cf650d0734858ef84d2ffa5
Dpi-001 <sudip9845197918@gmail.com>
2025-03-17 11:02:31 +0545

    For Loop Iteration

---
+++ ForLoop.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class ForLoop
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}

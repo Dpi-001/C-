291343b4eac025b9b5849fc1412b9ff45a88f69b
Dpi-001 <sudip9845197918@gmail.com>
2025-03-24 10:45:57 +0545

    Hello world in NameSpace

---
+++ Namesp.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyNameSpace
+{
+    public class Myclass
+    {
+        public void display()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ namespa.cs
@@ -0,0 +1,12 @@
+using System;
+
+using MyNameSpace;
+
+class program
+{
+    public static void Main(string[] args)
+    {
+        Myclass obj = new Myclass();
+        obj.display();
+    }
+}

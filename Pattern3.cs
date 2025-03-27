5771a69420429b3c49b3f97276ec090c7d6cdd09
Dpi-001 <sudip9845197918@gmail.com>
2025-03-27 11:02:51 +0545

    1 22 333 4444 55555

---
+++ Pattern3.cs
@@ -0,0 +1,16 @@
+using System;
+
+class PatternProgram
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
